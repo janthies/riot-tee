eb2b050924598df8acb1537c105fb068bf9b0d26
Jan Thies <janthies00@gmail.com>
2026-03-17 09:55:19 +0100

    single source of truth for memory layout

--- src/main.c
+++ src/main.c
@@ -17,9 +17,9 @@
  */
 
 #include <stdint.h>
-#include <stddef.h>                     // Defines NULL
-#include <stdbool.h>                    // Defines true
-#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include <stddef.h>  // Defines NULL
+#include <stdbool.h> // Defines true
+#include <stdlib.h>  // Defines EXIT_FAILURE
 #include <stdio.h>
 
 #include <arm_cmse.h>
@@ -33,97 +33,107 @@
 #include "tee_random.h"
 #include "tee_rot.h"
 
-extern unsigned int FLASH_START_NS;
+#include "nrf9160.h"
+
+extern unsigned int FLASH_START_NS[];
 
 /* Define the start of the non-secure vector table */
-const unsigned long TZ_START_NS = (unsigned int) (&FLASH_START_NS);
+const unsigned long TZ_START_NS = (unsigned int)(FLASH_START_NS);
 
 /* Define the function pointer type for the non-secure reset handler */
 typedef int __attribute__((cmse_nonsecure_call)) nsfunc(void);
 
 int main(void)
 {
-    /* Flash is separated into 32 * 32KB regions.
-       The first four (128KB) are secure, all others are NS */
-    for (uint32_t i = 4; i < 32; i++) {
-        /* Initial value is 0x0001 0101
-           NS value is 0x0000 0101 */
-        NRF_SPU_S->FLASHREGION[i].PERM &=
-            ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
-    }
+   /* Flash is separated into 32 * 32KB regions.
+      Secure regions are defined by FIRST_NS_FLASH_REGION, all others are NS */
+   for (uint32_t i = FIRST_NS_FLASH_REGION; i < 32; i++)
+   {
+      /* Initial value is 0x0001 0101
+         NS value is 0x0000 0101 */
+      NRF_SPU_S->FLASHREGION[i].PERM &=
+          ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
+   }
 
-    /* RAM is separated into 32 * 8 KB regions.
-       The first 8 are secure, all others are NS */
-    for (uint32_t i = 11; i < 32; ++i) {
-        /* Initial value is 0x0001 0101
-           NS value is 0x0000 0101 */
-        NRF_SPU_S->RAMREGION[i].PERM &=
-            ~(SPU_RAMREGION_PERM_SECATTR_Msk);
-    }
+   /* RAM is separated into 32 * 8 KB regions.
+      Secure regions are defined by FIRST_NS_RAM_REGION, all others are NS */
+   for (uint32_t i = FIRST_NS_RAM_REGION; i < 32; ++i)
+   {
+      /* Initial value is 0x0001 0101
+         NS value is 0x0000 0101 */
+      NRF_SPU_S->RAMREGION[i].PERM &=
+          ~(SPU_RAMREGION_PERM_SECATTR_Msk);
+   }
 
-    /* NSC flash region needs to be configured at the
-       end of the last flash region.
-       There are two possible NSC regions, so
-       flash_nsc_id can be 0 or 1. We only have one
-       region, so we use ID 0 */
-    int flash_nsc_id = 0;
-    int flash_region = 3;
+   /* NSC flash region needs to be configured at the
+      end of the last flash region.
+      There are two possible NSC regions, so
+      flash_nsc_id can be 0 or 1. We only have one
+      region, so we use ID 0 */
+   int flash_nsc_id = 0;
 
-    /* We configure the end of flash region 1 as NSC
-       NSC region is 32B, which is the smallest possible size */
-    NRF_SPU_S->FLASHNSC[flash_nsc_id].REGION = flash_region;
-    NRF_SPU_S->FLASHNSC[flash_nsc_id].SIZE = NRF_SPU_NSC_SIZE_32B;
+   /* We configure the end of the last secure flash region as NSC
+      NSC region is 32B, which is the smallest possible size */
+   NRF_SPU_S->FLASHNSC[flash_nsc_id].REGION = NSC_FLASH_REGION;
+   NRF_SPU_S->FLASHNSC[flash_nsc_id].SIZE = NRF_SPU_NSC_SIZE_32B;
 
-    /* Configure GPIO P0 for NS access */
-    NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_P0_NS)].PERM &= ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
+   /* Configure GPIO P0 for NS access */
+   NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_P0_NS)].PERM &= ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
 
-    /* Configure UARTE0 for NS access */
-    NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_UARTE0_NS)].PERM &= ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
+   /* Configure UARTE0 for NS access */
+   NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_UARTE0_NS)].PERM &= ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
 
-    /* Configure TIMER0 for NS access */
-    NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_TIMER0_NS)].PERM &= ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
+   /* Configure TIMER0 for NS access */
+   NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_TIMER0_NS)].PERM &= ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
 
-    /* Configure TIMER1 for NS access */
-    NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_TIMER1_NS)].PERM &= ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
+   /* Configure TIMER1 for NS access */
+   NRF_SPU_S->PERIPHID[NRFX_PERIPHERAL_ID_GET(NRF_TIMER1_NS)].PERM &= ~(SPU_FLASHREGION_PERM_SECATTR_Msk);
 
-    /* Set GPIO P0 pin attributes to 0 (= non-secure) */
-    NRF_SPU_S->GPIOPORT[0].PERM = 0x00000000ul;
+   /* Set GPIO P0 pin attributes to 0 (= non-secure) */
+   NRF_SPU_S->GPIOPORT[0].PERM = 0x00000000ul;
 
-    /* Make sure floating point registers are cleared when returning to non-secure world */
-    FPU->FPCCR |= FPU_FPCCR_TS_Msk | FPU_FPCCR_CLRONRET_Msk | FPU_FPCCR_CLRONRETS_Msk;
+   NRF_P0_S->DIRSET |= (1 << 7);
+   NRF_P0_S->OUTCLR |= (1 << 7);
 
-    /* Raise NS exception priority to 0x80 to prevent preemption of secure fault exceptions */
-    SCB->AIRCR |= SCB_AIRCR_PRIS_Msk;
+   /* Make sure floating point registers are cleared when returning to non-secure world */
+   FPU->FPCCR |= FPU_FPCCR_TS_Msk | FPU_FPCCR_CLRONRET_Msk | FPU_FPCCR_CLRONRETS_Msk;
 
-    /* Initialize the random number generator */
-    CYS_error_t status = tee_init_random();
-    if (status != CYS_SUCCESS) {
-        puts("Random number generator initialization failed");
-    }
+   /* Raise NS exception priority to 0x80 to prevent preemption of secure fault exceptions */
+   SCB->AIRCR |= SCB_AIRCR_PRIS_Msk;
 
-    /* Initialize the CryptoCell */
-    NRF_CRYPTOCELL->ENABLE = 1;
-    cc3xx_lowlevel_init();
-    NRF_CRYPTOCELL->ENABLE = 0;
+   /* Initialize the random number generator */
+   CYS_error_t status = tee_init_random();
+   if (status != CYS_SUCCESS)
+   {
+      puts("Random number generator initialization failed");
+   }
 
-    status = tee_rot_try_generate_aes_key();
-    if (status != CYS_SUCCESS && status != CYS_ERROR_ALREADY_EXISTS) {
-        puts("AES Platform key generation failed");
-    }
+   /* Initialize the CryptoCell */
+   NRF_CRYPTOCELL->ENABLE = 1;
+   cc3xx_lowlevel_init();
+   NRF_CRYPTOCELL->ENABLE = 0;
 
-    /* Write NS vector table to SCB_NS->VTOR to be able to jump to NS image*/
-    SCB_NS->VTOR = TZ_START_NS;
-    uint32_t* vtor = (uint32_t*)TZ_START_NS;
+   status = tee_rot_try_generate_aes_key();
+   if (status != CYS_SUCCESS && status != CYS_ERROR_ALREADY_EXISTS)
+   {
+      puts("AES Platform key generation failed");
+   }
 
-    /* Set non-secure main stack pointer to beginning of NS stack */
-    __TZ_set_MSP_NS(vtor[0]);
+   /* Write NS vector table to SCB_NS->VTOR to be able to jump to NS image*/
+   SCB_NS->VTOR = TZ_START_NS;
+   uint32_t *vtor = (uint32_t *)TZ_START_NS;
 
-    /* Call the non-secure reset handler */
-    nsfunc *ns_reset_handler = (nsfunc*)(vtor[1]);
-    ns_reset_handler();
+   /* Set non-secure main stack pointer to beginning of NS stack */
+   __TZ_set_MSP_NS(vtor[0]);
 
-    while (1) {}
+   /* Call the non-secure reset handler */
+   nsfunc *ns_reset_handler = (nsfunc *)(vtor[1]);
+   ns_reset_handler();
 
-    /* Execution should not come here during normal operation */
-    return ( EXIT_FAILURE );
+   while (1)
+   {
+   }
+
+   /* Execution should not come here during normal operation */
+   return (EXIT_FAILURE);
 }
